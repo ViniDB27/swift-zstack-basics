a084dd96ddbe0fed74288b0395edbee5da83162d
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 15:27:01 -0300

    Initial Commit

---
+++ zstack-basics/zstack_basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  zstack_basicsApp.swift
+//  zstack-basics
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct zstack_basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
